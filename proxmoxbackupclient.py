87815afbb58bd1f62f6e81228b1b0d00a35712c9
Qetesh <27067788+Qeteshpony@users.noreply.github.com>
2024-06-30 02:23:03 +0200

    feat: workaround for incorrect timestamps
    When checked while a backup is running, the backup client does not always report the latest backup time. The script now stores the timestamps of the last backups and compares them with the current value to prevent false error messages.

--- proxmoxbackupclient.py
+++ proxmoxbackupclient.py
@@ -12,6 +12,8 @@ from datetime import datetime
 config = configparser.ConfigParser()
 config.read("/root/proxmoxbackupclient.ini")
 
+datafile = "/tmp/backupstatus.json"
+
 # warn and critical levels for backup-age in seconds
 warnage = 60*60*24 + 60*60  # 1 day and 1 hour
 critage = 60*60*24*7 + 60*60  # 1 week and 1 hour
@@ -30,6 +32,14 @@ def getClientData() -> dict:
         exit(1)
 
 if __name__ == "__main__":
+    try:
+        # load stored timestamps if possible
+        with open(datafile, "r") as f:
+            storeddata = json.load(f)
+    except FileNotFoundError:
+        storeddata = {}
+    changed = False
+
     data = getClientData()
     for host in data:
         # get the data for each stored backup and process it
@@ -39,6 +49,17 @@ if __name__ == "__main__":
             status = 2
             date = "unknown"
         else:
+            # compare reported timestamp to stored one and update/overwrite if necessary
+            if hostname in storeddata.keys():
+                if timestamp < storeddata[hostname]:
+                    timestamp = storeddata[hostname]
+                elif timestamp > storeddata[hostname]:
+                    storeddata[hostname] = timestamp
+                    changed = True
+            else:
+                storeddata[hostname] = timestamp
+                changed = True
+
             date = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
             if timestamp + critage < time.time():
                 status = 2
@@ -47,7 +68,7 @@ if __name__ == "__main__":
             else:
                 status = 0
         print(f'{status} "Backup Status {hostname}" - Last backup: {date}, Backup-Count: {host.get("backup-count")}, Owner: {host.get("owner")}')
-
-
-
-
+    if changed:
+        # store the new data if there were any changes
+        with open(datafile, "w") as f:
+            json.dump(storeddata, f)
